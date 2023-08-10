89b76da71186a5a434d205bd5bfbe69e6f161c22
Sys-Admiral <Sys-Admiral@K415-1>
2023-08-10 10:27:13 +0300

    Добавьте файлы проекта.

---
+++ TestTaskRW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestTaskRW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
